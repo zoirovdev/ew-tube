cfc4e1e0fd7369f2d1da188ae3afde714befb3bf
zoirovdev <abboszoirov17@gmail.com>
2025-05-17 22:29:48 +0500

    first commit

---
+++ src/components/app/app.jsx
@@ -0,0 +1,15 @@
+import { Stack, Button, Container } from '@mui/material'
+
+const App = () => {
+    return (
+    <Container>
+        <Stack spacing={2} direction="row">
+            <Button variant="text">Text</Button>
+            <Button variant="contained">Contained</Button>
+            <Button variant="outlined">Outlined</Button>
+        </Stack>
+    </Container>)
+}
+
+
+export default App
