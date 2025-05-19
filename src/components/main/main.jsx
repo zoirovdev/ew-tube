b30b9348a66162aa79c578597a052b2572dfb4dd
zoirovdev <abboszoirov17@gmail.com>
2025-05-19 09:22:30 +0500

    add category component

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,11 +1,22 @@
 import { Link } from 'react-router-dom'
-import { Button } from '@mui/material'
+import { Stack, Box, Typography, Container } from '@mui/material'
+import { useState } from 'react'
+import { colors } from '../../constants/colors'
+import { Category } from '../'
 
 const Main = () => {
+    const [selectedCategory, setSelectedCategory] = useState('New')
     return (
-        <Link to={'/channel'}>
-            <Button>Channel</Button>
-        </Link>
+	<Stack>
+	    <Category/>
+	    <Box p={2} sx={{ height: '90vh' }}>
+		<Container maxWidth={"90%"}>
+		    <Typography variant={'h4'} fontWeight={'bold'} mb={2}>
+	    		{ selectedCategory } <span style={{ color: colors.secondary }}>videos</span>
+		    </Typography>
+	    	</Container>
+	    </Box>
+	</Stack>
     )
 }
 
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,32 @@
+import { 
+    Checkroom,
+    DeveloperMode,
+    FaceRetouchingNatural,
+    FitnessCenter,
+    GraphicEq,
+    Home, 
+    LiveTv,
+    MusicNote,
+    OndemandVideo,
+    School,
+    SportsEsports,
+    TheaterComedy
+} from '@mui/icons-material'
+
 export const logo = 'https://dynamic.design.com/asset/logo/3d519db4-441e-4fab-b8c1-e0d3534146ba/logo-search-grid-1x?logoTemplateVersion=1&v=638086440065200000&text=star'
+
+export const category = [
+    { name: 'New', icon: <Home/> },
+    { name: 'Movie', icon: <OndemandVideo/> },
+    { name: 'Live', icon: <LiveTv/> },
+    { name: 'Gaming', icon: <SportsEsports/> },
+    { name: 'Education', icon: <School/> },
+    { name: 'Sport', icon: <FitnessCenter/> },
+    { name: 'Comedy', icon: <TheaterComedy/> },
+    { name: 'Podcast', icon: <GraphicEq/> },
+    { name: 'Fashion', icon: <Checkroom/> },
+    { name: 'Crypto', icon: <DeveloperMode/> },
+    { name: 'Gym', icon: <FitnessCenter/> },
+    { name: 'Beauty', icon: <FaceRetouchingNatural/> },
+    { name: 'Music', icon: <MusicNote/> }
+]
