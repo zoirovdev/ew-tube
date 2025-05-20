fb5f2015036bce5422414bc4e7287971bacc0cfb
zoirovdev <abboszoirov17@gmail.com>
2025-05-20 11:44:54 +0500

    add video card

--- src/components/videos/videos.jsx
+++ src/components/videos/videos.jsx
@@ -1,5 +1,23 @@
+import { Stack, Box } from '@mui/material'
+import { VideoCard } from '../'
+
+
 const Videos = ({ videos }) => {
-  return 
+  return (
+    <Stack
+      width={'100%'}
+      direction={'row'}
+      flexWrap={'wrap'}
+      justifyContent={'start'}
+      alignItems={'center'}
+      gap={2}>
+	{videos.map(item => (
+	  <Box key={item.id.videoId}>
+	    {item.id.videoId && <VideoCard video={item}/>}
+	  </Box>
+	))}
+    </Stack>
+  )
 }
 
 
